8a5adbcea7a1e000bfcee6b12c8c17244d7c2369
Thales Batista <thalesbatista18@gmail.com>
2021-05-17 10:14:29 -0300

    Atualização do projeto de Angular parte 1

---
+++ Angular/bytebank/src/app/nova-transferencia/nova-transferencia.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-nova-transferencia',
+    templateUrl: './nova-transferencia.component.html',
+    styleUrls: ['./nova-transferencia.component.scss']
+})
+
+export class NovaTransferenciaComponent {
+
+  valor: number;
+  destino: number;
+
+  transferir(): void {
+    console.log('Solicitada nova transferência');
+    console.log('valor: ', this.valor);
+    console.log('destino: ', this.destino);
+  }
+
+}
